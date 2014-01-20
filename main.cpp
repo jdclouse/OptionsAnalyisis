68b7c3b6aec8a2517cc858b6d6df304d1cbd6395
John Clouse <clous0dd@gmail.com>
2014-01-19 22:12:05 -0700

    Added call option testing, initial test of Iron Condor

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "src/Put.h"
+#include "src/Call.h"
 #include <iostream>
 
 int main() {
@@ -22,6 +23,22 @@ int main() {
 	for (unsigned int ii = 24; ii < 33; ++ii) {
 		test_put2.setEndPrice(ii);
 		std::cout << ii << ": " << test_put2.calculateBuy() << std::endl;
+	}
+	Put condor_buy_put(45, 35, 35, 50, 100);
+	Put condor_sell_put(45, 40, 35, 100, 100);
+	Call condor_buy_call(45, 55, 35, 50, 100);
+	Call condor_sell_call(45, 50, 35, 100, 100);
+	std::cout << "IC low end:" << std::endl;
+	for (unsigned int ii = 30; ii < 60; ++ii) {
+		condor_buy_put.setEndPrice(ii);
+		condor_sell_put.setEndPrice(ii);
+		condor_buy_call.setEndPrice(ii);
+		condor_sell_call.setEndPrice(ii);
+		double long_put = condor_buy_put.calculateBuy();
+		double short_put = condor_sell_put.calculateSell();
+		double long_call = condor_buy_call.calculateBuy();
+		double short_call = condor_sell_call.calculateSell();
+		std::cout << ii << ": " << long_put << ", " << short_put << ", " << long_call << ", " << short_call << ", " << long_put + short_put + long_call + short_call << std::endl;
 	}
 	return 0;
 }
